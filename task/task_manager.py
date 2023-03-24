2b8139fcda170cb17cbf4b0979d1703ec055098b
linmao78730 <linmao78730@gmail.com>
2023-03-24 08:10:14 -0400

    fixed gitignore

--- task/task_manager.py
+++ task/task_manager.py
@@ -1,13 +1,19 @@
 from task.task_config import *
+from task.task_executor import *
 import threading
 from typing import Dict, List, Union
 import json
 
 
+
 class Task:
     def __init__(self,
-                 task_id:str=None):
+                 task_id:str=None,
+                 task_config:BasicTaskConfig=None,
+                 task_result:TaskResult=None):
         self.task_id = task_id
+        self.task_config = task_config
+        self.task_result = task_result
 
 
 class TaskManager:
