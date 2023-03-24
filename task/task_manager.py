9959d0b9e13c0e369523f8735bb5317a848f39ce
linmao78730 <linmao78730@gmail.com>
2023-03-24 08:23:35 -0400

    fixed gitignore

--- task/task_manager.py
+++ task/task_manager.py
@@ -11,11 +11,22 @@ class Task:
                  task_id:str=None,
                  task_config:BasicTaskConfig=None,
                  task_result:TaskResult=None):
-        self.task_id = task_id
-        self.task_config = task_config
-        self.task_result = task_result
+        self.__task_id = task_id
+        self.__task_config = task_config
+        self.__task_result = task_result
+        return
+
+    def task_id(self) -> str:
+        return self.__task_id
+
+    def task_config(self) -> BasicTaskConfig:
+        return self.__task_config
+
+    def task_result(self) -> TaskResult:
+        return self.__task_result
 
 
 class TaskManager:
-    def __init__(self):
+    def __init__(self,
+                 saved_tasks_file:str):
         self.all_tasks = {}
