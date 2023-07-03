fc6392b9c3fde59bd13ac42623f9e878effe6e57
linmao78730 <linmao78730@gmail.com>
2023-07-03 15:51:23 -0400

    modified by Mao

--- run_simple_client.sh
+++ run_simple_client.sh
@@ -2,5 +2,6 @@ clear
 
 export CLIENT_ADDR=$1
 export CLIENT_PORT=$2
+export PYTHON_HOME=$3
 
 python simple_client.py
--- run_simple_server.sh
+++ run_simple_server.sh
@@ -3,5 +3,6 @@ clear
 export SOURCE_HOME=$1
 export SERVER_ADDR=$2
 export SERVER_PORT=$3
+export PYTHON_HOME=$4
 
 python simple_server.py
